ba42dd93d295147c2c8de8eb3ccdc92f8da43525
Sergey Seroukhov <seroukhov@gmail.com>
2017-02-19 20:56:46 -0800

    'Initial commit'

---
+++ workspace.conf.ps1
@@ -0,0 +1,8 @@
+$VersionControl = 'git'
+$VersionControlRepos = @(
+    "https://github.com/pip-benchmark/pip-benchmark-dotnet.git"
+    "https://github.com/pip-benchmark/pip-benchmark-java.git"    
+    "https://github.com/pip-benchmark/pip-benchmark-node.git"
+    "https://github.com/pip-benchmark/pip-benchmark-python.git"        
+    "https://github.com/pip-benchmark/pip-benchmark-go.git"
+)
